58acdc8013f28e814252af9163b98be42f89eb0b
lucas-xu51 <hxu51@student.ubc.ca>
2024-03-16 16:58:36 -0700

    signup page

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ from flask import Flask
 from flask import request, render_template, redirect, url_for, session, g
 import sqlite3
 import threading
+
+import os
+import time
+
+from flask import Flask, render_template, request, send_from_directory
+
+
 con = sqlite3.connect("tutorial.db",check_same_thread=False)
 cur = con.cursor()
 id=0
@@ -13,22 +20,11 @@ lock = threading.Lock()
 # con.commit()
 
 
-
 app = Flask(__name__, static_url_path="/")
 app.config['SECRET_KEY'] = "sdfklas0lk42j"
 
 
-@dataclass
-class User:
-    id: int
-    username: str
-    password: str
 
-users = [
-	User(1, "Admin", "123456"),
-	User(2, "Eason", "888888"),
-	User(3, "Tommy", "666666"),
-]
 
 @app.before_request
 def before_request():
@@ -75,20 +71,80 @@ def profile():
     return render_template("profile.html")
 
 
-#signup page
-@app.route("/signup")
+# signup
+@app.route("/signup",methods=['GET', 'POST'])
 def signup():
-    # if request.method == 'POST':
-    #     # sign up
-    #     session.pop('user_id', None) #clean before signup
-    #     username = request.form.get("username", None)
-    #     password = request.form.get("password", None)
-    #     # role = 1/2/3?
-    #     data=[(id,username,password)]
-    #     try:
-    #         lock.acquire(True)
-    #         user = cur.execute("INSERT INTO users VALUES(?, ?, ?)", data) #insert user into db
-    #     finally:
-    #         lock.release()
-    print("sign up")
+    if request.method == 'POST':
+        # sign up
+        session.pop('user_id', None) #clean before signup
+        username = request.form.get("username", None)
+        password = request.form.get("password", None)
+        # role = 1/2/3?
+        data=(1,username,password)
+        try:
+            lock.acquire(True)
+            user = cur.execute("INSERT INTO users VALUES(?,?,?)",data) #insert user into db
+            con.commit()
+        finally:
+            lock.release()
+        return redirect(url_for('login'))
     return render_template("signup.html")
+
+
+
+
+# upload file
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    return render_template('upload.html')
+
+app.config['UPLOAD_FOLDER'] = r'C:\Users\Haozhe XU\Desktop\COSC310\server'
+file_dir = app.config['UPLOAD_FOLDER']
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def uploader():
+    """
+        upload file
+    """
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+        print(request.files, f.filename)
+
+        return 'upload successfully!'
+    else:
+        return render_template('upload.html')
+    
+
+# download file
+@app.route('/download', methods=['GET', 'POST'])
+def download():
+    """
+        download file
+    :return:
+    """
+    timelist = []   
+    Foder_Name = []     
+    Files_Name = []  
+
+    lists = os.listdir(r'C:\Users\Haozhe XU\Desktop\COSC310\server')
+
+    for i in lists:
+        timelist.append(time.ctime(os.path.getatime(r'C:\Users\Haozhe XU\Desktop\COSC310\server' + '\\' + i)))
+
+    for k in range(len(lists)):
+        Files_Name.append(lists[k])
+
+    print(r'C:\Users\Haozhe XU\Desktop\COSC310\server') 
+
+    return render_template('download.html', allname=Foder_Name, name=Files_Name)
+
+
+@app.route('/downloads/<path:path>', methods=['GET', 'POST'])
+def downloads(path):
+    """
+        download file
+    :param path:
+    :return:
+    """ 
+    return send_from_directory(r'C:\Users\Haozhe XU\Desktop\COSC310\server', path, as_attachment=True)
